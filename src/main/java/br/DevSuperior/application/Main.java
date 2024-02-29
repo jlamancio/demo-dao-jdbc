a91a9ff1347502f0be578a84c955e70415a18a61
jlamancio <amancio.jluis@outlook.com>
2024-02-29 19:15:44 -0300

    DAO - Create Department entity class

--- src/main/java/br/DevSuperior/application/Main.java
+++ src/main/java/br/DevSuperior/application/Main.java
@@ -1,7 +1,10 @@
 package br.DevSuperior.application;
 
+import br.DevSuperior.model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
