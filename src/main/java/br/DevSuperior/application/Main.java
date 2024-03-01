94af380fc9171dc492209dadeed5b708630b7100
jlamancio <amancio.jluis@outlook.com>
2024-03-01 06:33:03 -0300

    DAO - Create Seller entity class

--- src/main/java/br/DevSuperior/application/Main.java
+++ src/main/java/br/DevSuperior/application/Main.java
@@ -1,10 +1,17 @@
 package br.DevSuperior.application;
 
 import br.DevSuperior.model.entities.Department;
+import br.DevSuperior.model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Department department = new Department(1, "Books");
+        Seller seller = new Seller(21, "José Luis Amancio", "amancio@email.com",
+                new Date(), 3000.00, department);
+
+        System.out.println(seller);
     }
 }
