34d9a58b413df1bb384e5fd843f34bf3249b9fd3
jlamancio <amancio.jluis@outlook.com>
2024-03-11 20:32:17 -0300

    DAO - Create DepartmentDao and Factory

--- src/main/java/br/DevSuperior/model/dao/DaoFactory.java
+++ src/main/java/br/DevSuperior/model/dao/DaoFactory.java
@@ -1,11 +1,15 @@
 package br.DevSuperior.model.dao;
 
 import br.DevSuperior.db.DB;
+import br.DevSuperior.model.dao.impl.DepartmentDaoJDBC;
 import br.DevSuperior.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
+    }
+    public static DepartmentDao createDepartmentDao(){
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
